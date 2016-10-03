cd67bcae66cfcc3d14b287b4821157f639b407f5
Michael Smykowski <smycal@gmail.com>
2016-10-03 16:38:15 -0700

    Add phnatom and cli test runner

--- tasks/jasmine.js
+++ tasks/jasmine.js
@@ -1,29 +1,39 @@
-var gulp = require('gulp');
-var jasmineBrowser = require('gulp-jasmine-browser');
-var webpack = require('webpack-stream');
- 
-gulp.task('jasmine', function() {
-  var JasminePlugin = require('gulp-jasmine-browser/webpack/jasmine-plugin');
-  var plugin = new JasminePlugin();
-  return gulp.src(['spec/**/*_spec.js'])
-    .pipe(webpack({watch: true,
-      module: {
-        loaders: [
+const gulp = require('gulp');
+const jasmineBrowser = require('gulp-jasmine-browser');
+const webpack = require('webpack-stream');
+
+const moduleLoaders = {loaders: [
         { test: /\.css$/, loader: "style!css" },
         {
           test: /\.js$/,
           exclude: /(node_modules|bower_components)/,
           loader: 'babel-loader',
-          query: {
-            presets: [
-              'es2015', 
-              'react'
-            ]
-          }
-        }]
-      },
+          query: {presets: ['es2015', 'react']}
+        }
+      ]
+    };
+ 
+gulp.task('jasmine', function() {
+  const JasminePlugin = require('gulp-jasmine-browser/webpack/jasmine-plugin');
+  const plugin = new JasminePlugin();
+  return gulp.src(['spec/**/*_spec.js'])
+    .pipe(webpack({watch: true,
+      module: moduleLoaders,
       output: {filename: 'spec.js'}, 
       plugins: [plugin]}))
     .pipe(jasmineBrowser.specRunner())
+    .pipe(jasmineBrowser.server({whenReady: plugin.whenReady}));
+});
+
+gulp.task('spec', function() {
+  const JasminePlugin = require('gulp-jasmine-browser/webpack/jasmine-plugin');
+  const plugin = new JasminePlugin();
+  return gulp.src(['spec/**/*_spec.js'])
+    .pipe(webpack({watch: false,
+      module: moduleLoaders,
+      output: {filename: 'spec.js'},
+      plugins: [plugin]}))
+    .pipe(jasmineBrowser.specRunner({console: true}))
+    .pipe(jasmineBrowser.headless({driver: 'phantomjs'}))
     .pipe(jasmineBrowser.server({whenReady: plugin.whenReady}));
 });
