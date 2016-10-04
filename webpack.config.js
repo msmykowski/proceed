835394473a8998208e8b81d5f32755244a48d375
Michael Smykowski <smycal@gmail.com>
2016-10-03 17:26:24 -0700

    Add production configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+let presets = ['es2015', 'react'];
+if (process.env.NODE_ENV !== 'production') presets.push('react-hmre');
+
+
 module.exports = {
   entry: "./app/javascripts/main.js",
   output: {
       path: require('path').resolve(__dirname, 'static/'),
       filename: "bundle.js"
   },
-  devServer:{contentBase: 'static'},
+  devServer: {contentBase: 'static'},
   module: {
     loaders: [
         { test: /\.css$/, loader: "style!css" },
@@ -13,11 +17,7 @@ module.exports = {
           exclude: /(node_modules|bower_components)/,
           loader: 'babel-loader',
           query: {
-            presets: [
-              'es2015', 
-              'react',
-              'react-hmre'
-            ]
+            presets
           }
         }
       ]
